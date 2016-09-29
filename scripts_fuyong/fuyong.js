2c4e33299da5024e1b63ba04a300d60e7e19db74
Lee <1049188593@qq.com>
2016-09-29 21:48:25 +0800

    change some

--- scripts_fuyong/fuyong.js
+++ scripts_fuyong/fuyong.js
@@ -31,9 +31,6 @@ function addLoadEvent(func) {
 var CookieUtil = {
 	//获取cookie
 //获得cookies, 课件代码
-
-
-
 	unset:function(name,path,domain,secure)//删除
 	{
 		this.set(name,' ',new Date(0),path,domain,secure);//data(0)  
@@ -134,4 +131,62 @@ function submitFormWithAjax( whichform, thetarget ) {
         }
     }
   };
+}
+
+/**
+ * [get description]
+ * @param  {String}   url      请求url地址 如var url = "http://study.163.com/webDev/couresByCategory.htm" ;
+ * @param  {Object}   options  请求参数为js字面量对象形式 如var options = {pageNo:1,psize:20,type:10};
+ * @param  {Function} callback 请求成功回调的函数必须传入一个参数表示接受xhr.responseText
+ * 
+ */
+function get(url,options,callback) {
+    //1.创建xhr对象
+    var xhr = new XMLHttpRequest();
+    //2. 在open方法之前监听redaystatechange
+    xhr.onreadystatechange = function () {
+        //2.1 判断readyState==4
+        if(xhr.readyState==4){
+            //2.2 判断状态码
+            if( (xhr.status>=200 && xhr.status<300) || xhr.status==304 ){
+                callback(xhr.responseText);
+            }
+            else{
+                console.log("ajax请求不成功,错误状态码为："+xhr.status);
+            }
+        }   
+    }
+    //如果传入的参数不为空
+    if(!!options){
+        url = url+"?"+serialize(options);
+        console.log("请求参数经过序列化后的url地址："+url);
+    }
+    //请求参数序列化的方法
+    function serialize(data) {
+        if(!data){//再次对参数判断确保返回为字符串
+            return "";
+        }
+        var arry = [];
+        for(var name in data){
+            //如果name属性不是data原型对象上的属性或则name.value属性值为function则跳出循环。
+            if( (!data.hasOwnProperty(name)) || typeof data[name]==="function" ){
+                continue ;
+            }
+            //求出value值
+            var value = data[name].toString();
+            //对name 和进行编码
+            name = encodeURIComponent(name);
+            value = encodeURIComponent(value);
+            var item = name + "=" + value;
+            arry.push(item);
+        }
+        return arry.join("&");
+    }
+    //3.open方法  url地址要加上option序列化
+    xhr.open("get",url,true);
+    //xhr.setReuestHeader("Content-Type","application/x-www-form-urlencoded");头部信息表单编码 可以省略
+    //xhr.setReuestHeader("MyHeader","MyValue");自定义头部信息 可以省略
+    //4.send方法
+    xhr.send(null);//get方法必须传入null
+    //如果是post请求则为send(serialize(formdata));
 }
