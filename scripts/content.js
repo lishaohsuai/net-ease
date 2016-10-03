9121216732c870ee464f3e1a1205e1bcd8173f9d
Lee <1049188593@qq.com>
2016-10-03 16:26:34 +0800

    bigchange

--- scripts/content.js
+++ scripts/content.js
@@ -81,6 +81,7 @@ function viewBig (argument) {
 	var view_big_img = {};//图片的容器
 	var view_big_details = {};//下面的介绍
 	var view_big_li = [];//
+	var indiv = {}; 
 
 	function callback (argument) {
 		super_sen = JSON.parse(argument);
@@ -88,18 +89,20 @@ function viewBig (argument) {
 		var con = document.getElementsByClassName('content-details')[0];
 		for(i = 0;i<20;i++)
 		{
+			
 			view_big_div[i] = document.createElement('div');//创建DIV
 			view_big_img[i] = document.createElement('img');//创建img
-			
+			indiv[i] = document.createElement('div');//创建DIV
 
 			view_big_details[i] = document.createElement('ul');//创建DIV
-
+			view_big_details[i].className = 'smallul';
 			view_big_li[i]=new Array();
 			for(j = 0; j < 4;j++)
 			{
 				view_big_li[i][j] = document.createElement('li');//创建li
 				view_big_details[i].appendChild(view_big_li[i][j]);
 			}
+
 
 			{
 				view_big_li[i][0].innerHTML = super_sen.list[i].name;
@@ -119,20 +122,17 @@ function viewBig (argument) {
 			//设置图片内容
 			view_big_img[i].className = 'bigImg';
 			view_big_img[i].setAttribute('src', super_sen.list[i].bigPhotoUrl);
-			view_big_img[i].onmouseover = function  (argument) {
-			}
 
-			
-			
 			view_big_div[i].className = 'blocks';
-
-
+			indiv[i].className = 'indiv';
+			indiv[i].appendChild(view_big_div[i])
 			view_big_div[i].appendChild(view_big_img[i]);
 			view_big_div[i].appendChild(view_big_details[i]);
-			con.appendChild(view_big_div[i]);
+			con.appendChild(indiv[i]);
 
 			
 		}
+		everymodule (super_sen);
 		//对ul添加时事件 标题颜色  加阴影
 			//mouseOver();
 		// 生成翻页器
@@ -178,11 +178,14 @@ function viewBig (argument) {
 				pages[0].style.backgroundColor = '#9dd8b1';
 				spanUp.addEventListener('click', upDownEvent);//每个按键绑定事件
 				spanDown.addEventListener('click', upDownEvent);//每个按键绑定事件
+				//阴影 加上
 				for(i = 0;i<20;i++){
 					view_big_div[i].addEventListener('mouseover',mouseOver);
 					view_big_div[i].addEventListener('mouseout',mouseOut);
+					//变换样式
+					view_big_img[i].addEventListener('mouseover',mouseOverImg);
+					// view_big_img[i].addEventListener('mouseout',mouseOutImg);
 				}
-				
 
 	}
 	get('http://study.163.com/webDev/couresByCategory.htm',options,callback);
@@ -197,68 +200,68 @@ var total = 5;//总共的页码
 
 //addLoadEvent(listen_span);	//绑定事件
 function cPage () {
-var show = document.getElementsByClassName('showPages');
-var is = isNaN(parseInt(this.innerHTML));
-if(is == true){
-	if(this.innerHTML = '>' ){
-		pageN = parseInt(show[0].innerHTML);
-		for(i =0 ;i<8;i++){
-		show[i].style.backgroundColor = 'transparent';
+	var show = document.getElementsByClassName('showPages');
+	var is = isNaN(parseInt(this.innerHTML));
+	if(is == true){
+		if(this.innerHTML = '>' ){
+			pageN = parseInt(show[0].innerHTML);
+			for(i =0 ;i<8;i++){
+			show[i].style.backgroundColor = 'transparent';
+			}
+			show[0].style.backgroundColor = '#9dd8b1';//当前选中变色
 		}
-		show[0].style.backgroundColor = '#9dd8b1';//当前选中变色
-	}
-	else if(this.innerHTML ='<'){
-		pageN = parseInt(show[0].innerHTML);
-		for(i =0 ;i<8;i++){
-		show[i].style.backgroundColor = 'transparent';
+		else if(this.innerHTML ='<'){
+			pageN = parseInt(show[0].innerHTML);
+			for(i =0 ;i<8;i++){
+			show[i].style.backgroundColor = 'transparent';
+			}
+			show[0].style.backgroundColor = '#9dd8b1';//当前选中变色
 		}
-		show[0].style.backgroundColor = '#9dd8b1';//当前选中变色
-	}
-	else{
-		pageN = 1;
-	}
-	
-}else{
-	pageN = parseInt(this.innerHTML);
-	// //变色
-	for(i =0 ;i<8;i++){
-		show[i].style.backgroundColor = 'transparent';
-	}
-	this.style.backgroundColor = '#9dd8b1';//当前选中变色
-}
-
-
-
-var options = {pageNo:pageN,psize:20,type:typ};
-function callback (argument) {
-
-	var supers = JSON.parse(argument);
-	// console.log(super_sen);
-	var con = document.getElementsByClassName('content-details')[0];
-
-	var bigfir = document.getElementsByClassName('bigfir');//所有的模块的信息更新
-	var bigsec = document.getElementsByClassName('bigsec');
-	var bigthi = document.getElementsByClassName('bigthi');
-	var bigfor = document.getElementsByClassName('bigfor');
-	var imgtwn = document.getElementsByClassName('bigImg');
-
-	//内容更新
-	for(i = 0;i<20;i++)
-	{
-		imgtwn[i].setAttribute('src', supers.list[i].bigPhotoUrl);
-
-		bigfir[i].innerHTML = supers.list[i].name;
-		bigsec[i].innerHTML = supers.list[i].provider;
-		bigthi[i].innerHTML = supers.list[i].learnerCount;
-		bigfor[i].innerHTML = '￥'+supers.list[i].price.toFixed(2);
-		if(supers.list[i].price == 0){
-			bigfor[i].innerHTML = '免费';
-		}	
+		else{
+			pageN = 1;
+		}
+		
+	}else{
+		pageN = parseInt(this.innerHTML);
+		// //变色
+		for(i =0 ;i<8;i++){
+			show[i].style.backgroundColor = 'transparent';
+		}
+		this.style.backgroundColor = '#9dd8b1';//当前选中变色
 	}
 
 
-}
-get('http://study.163.com/webDev/couresByCategory.htm',options,callback);
+
+	var options = {pageNo:pageN,psize:20,type:typ};
+	function callback (argument) {
+
+		var supers = JSON.parse(argument);
+		// console.log(super_sen);
+		var con = document.getElementsByClassName('content-details')[0];
+
+		var bigfir = document.getElementsByClassName('bigfir');//所有的模块的信息更新
+		var bigsec = document.getElementsByClassName('bigsec');
+		var bigthi = document.getElementsByClassName('bigthi');
+		var bigfor = document.getElementsByClassName('bigfor');
+		var imgtwn = document.getElementsByClassName('bigImg');
+		var blocks = document.getElementsByClassName('blocks');
+		//内容更新
+		for(i = 0;i<20;i++)
+		{
+			imgtwn[i].setAttribute('src', supers.list[i].bigPhotoUrl);
+
+			bigfir[i].innerHTML = supers.list[i].name;
+			bigsec[i].innerHTML = supers.list[i].provider;
+			bigthi[i].innerHTML = supers.list[i].learnerCount;
+			bigfor[i].innerHTML = '￥'+supers.list[i].price.toFixed(2);
+			if(supers.list[i].price == 0){
+				bigfor[i].innerHTML = '免费';
+			}	
+		}
+		everymodule (supers) ;
+
+	}
+	get('http://study.163.com/webDev/couresByCategory.htm',options,callback);
 }
 
 //对两个tab 绑定事件
@@ -324,13 +327,53 @@ function upDownEvent () {
 		}
 	}
 }
+//鼠标在文字之上
 function mouseOver (argument) {
-	this.style.boxShadow = '2px 2px 2px rgb(150,150,150)';
+	this.parentNode.style.boxShadow = '2px 2px 2px rgb(150,150,150)';
 	this.firstChild.nextSibling.firstChild.style.color = '#39a030';
-
+	console.log('hello');
 }
 function mouseOut (argument) {
-	this.style.boxShadow = '2px 2px 2px rgb(230,230,230)';
+	this.parentNode.style.boxShadow = '2px 2px 2px rgb(230,230,230)';
 	this.firstChild.nextSibling.firstChild.style.color = 'black';
 }
 
+function mouseOutImg (argument) {
+	// body... 
+	this.style.display = 'none';
+	
+	
+}
+function mouseOverImg (argument) {
+	this.parentNode.parentNode.lastChild.style.display = 'inline-block';
+	// this.nextSibling.style.zIndex = '-1';
+}
+function everymodule (obj) {
+	var float = {};//新建对象
+	var i;	
+	var indiv = document.getElementsByClassName('indiv');	
+	for(i = 0;i<20;i++){
+		float[i] = document.createElement('div');
+		float[i].innerHTML = "<img></img><ul><li></li><li></li><li></li><li></li></ul><div><p></p></div>";	
+		float[i].className = 'bigblock';
+		// float[i].style.display = 'inline-block';
+		indiv[i].appendChild(float[i]);
+
+		//增加元素
+		float[i].firstChild.src = obj.list[i].bigPhotoUrl;//图片
+		float[i].firstChild.nextSibling.className = 'bigul';
+		float[i].firstChild.nextSibling.firstChild.innerHTML = obj.list[i].name;//标题
+		float[i].firstChild.nextSibling.firstChild.className = 'fir';
+		float[i].firstChild.nextSibling.firstChild.nextSibling.innerHTML = obj.list[i].learnerCount + '人在学';//obj.list[i].learnerCount
+		console.log(obj.list[i].learnerCount ) ;
+		float[i].firstChild.nextSibling.firstChild.nextSibling.className = 'sec';
+		float[i].firstChild.nextSibling.firstChild.nextSibling.nextSibling.innerHTML = '发布者:'+ obj.list[i].provider;
+		float[i].firstChild.nextSibling.firstChild.nextSibling.nextSibling.className = 'thi';
+		float[i].firstChild.nextSibling.lastChild.innerHTML = '分类:'+ obj.list[i].categoryName;
+		float[i].firstChild.nextSibling.lastChild.className = 'for';
+		float[i].lastChild.lastChild.innerHTML = obj.list[i].description;//课程藐视
+		float[i].lastChild.className = 'bigdiv';
+		float[i].addEventListener('mouseout',mouseOutImg);
+	}
+
+}
